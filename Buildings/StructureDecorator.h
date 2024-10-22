030bbf30ff89f4384c6319f62bf6dbc958fae2ae
Khomotjo246 <153485339+Khomotjo246@users.noreply.github.com>
2024-10-22 15:23:45 +0200

    added buildings folder

---
+++ Buildings/StructureDecorator.h
@@ -0,0 +1,29 @@
+#ifndef STRUCTUREDECORATOR_H
+#define STRUCTUREDECORATOR_H
+
+#include <iostream>
+#include "Structure.h"
+
+
+using namespace std;
+
+class StructureDecorator : public Structure
+{
+    private:
+        Structure* decoratedStructure;
+
+    protected:
+        Structure* decoratedStructure;
+
+    public:
+        StructureDecorator(Structure* structure);
+        virtual ~StructureDecorator();
+        virtual string getName() override;
+        virtual float getMaintenance() override;
+        virtual float getResourceConsumption() override;
+        virtual float getCitizenSatisfactionImpact() override;
+        
+
+};
+
+#endif

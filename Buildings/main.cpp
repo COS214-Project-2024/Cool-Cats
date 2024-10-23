0b9eae434c689ca8756d4ada832a23778cb94486
Khomotjo246 <153485339+Khomotjo246@users.noreply.github.com>
2024-10-23 10:07:10 +0200

    changes

---
+++ Buildings/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include "Structure.h"
+#include "StructureGroup.h"
+#include "StructureDecorator.h"
+#include "CStructIterator.h"
+#include "SatisfactionEnhancer.h"
+#include "MaintenanceCostReducer.h"
+#include "ResourceEfficiencyEnhancer.h"
+#include "BasicStructure.h"
+#include "StructureIterator.h"
+#include "CStructureGroup.h"
+
+
+
+void testStructure(){
+    BasicStructure building1 = new BasicStructure();
+
+
+}

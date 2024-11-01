b148d1376256d6944e1b08c5b2fe3bb52a5af4be
Sean-Mp <156788356+Sean-Mp@users.noreply.github.com>
2024-11-01 12:34:07 +0200

    Revert "Government pull request into dev"

--- government/TaxationCommand.cpp
+++
@@ -1,7 +0,0 @@
-#include "TaxationCommand.h"
-
-TaxationCommand::TaxationCommand(Government& govt, float rate) : government(govt), taxRate(rate) {}
-
-void TaxationCommand::execute() {
-    government.setTaxRate(taxRate);
-}

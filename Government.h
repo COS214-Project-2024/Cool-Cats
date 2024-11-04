6b71b7ac4f1253d768a0d81470c934bf03297ce5
ThabisoXavierMncube <u22617362@tuks.co.za>
2024-11-04 09:58:38 +0200

    government and taxes

---
+++ Government.h
@@ -0,0 +1,36 @@
+#ifndef GOVERNMENT_H
+#define GOVERNMENT_H
+
+#include <string>
+#include <vector>
+#include"../Citizen/AllCitizenIterator.h"
+#include "ResourceMediator.h"
+#include "TaxMemento.h"
+
+class Government {
+private:
+    Taxes* tax;
+    double budget;
+    std::vector<std::string> policies;
+    ResourceMediator* mediator;
+    std::vector<TaxMemento*> taxMementos;
+
+public:
+    Government(ResourceMediator* mediator);
+    ~Government();
+    void setTaxRate(float newRate);
+    void setTaxRate(Taxes* newRate);
+    Taxes* getTax();
+    void implementPolicy(const std::string& policy);
+    float getBudget() const;
+    void allocateBudget(float amount);
+    void collection(AllCitizenIterator *one);
+    void enforcePolicy(const std::string& policy);
+    void revokePolicy(const std::string& policy);
+    void requestResource(const std::string& resourceType, double amount);
+    void supplyResource(const std::string& resourceType, double amount);
+    void storeMemento(TaxMemento* memento);
+    TaxMemento* getMemento();
+};
+
+#endif
---
+++ Taxes.h
@@ -0,0 +1,24 @@
+#ifndef TAXES
+#define TAXES
+
+class TaxMemento;
+
+class Taxes {
+private:
+    float taxRate;
+    // float utilityCharge;
+    // float serviceFees;
+
+public:
+    Taxes(float one);
+    virtual ~Taxes() ;
+    void collection();
+    void allocation();
+    void impact() ;
+    float getTaxRate();
+    void setTaxRate(float n);
+    TaxMemento* createMemento();
+    void reinstateMemento(TaxMemento* n);
+};
+
+#endif
